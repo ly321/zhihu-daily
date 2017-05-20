30f08162c389838740d09f3cf045669b8937d550
ly321 <965768042@qq.com>
2017-05-20 23:07:12 +0800

    start

---
+++ zhihu-daily-vue/src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="header">
+        <div class="menu" @click="handleClick()"></div>
+        <span>{{title}}</span>
+    </div>
+</template>
+<script>
+    export default{
+        name:"header",
+        data(){
+            return{
+            }
+        },
+        methods:{
+            handleClick:function(){
+                this.onclick();
+            }
+        }
+    }
+</script>
+<style >
+    .header{
+        width: inherit;
+    height: 70px;
+    background-color: #00a2ea;
+    justify-content: flex-start;
+    padding-left: 30px;
+    flex-basis: 70px;
+    flex-shrink: 0;
+    }
+    .header .menu{
+            height: 16px;
+    width: 20px;
+    border-color: #fff;
+    border-style: solid;
+    border-width: 2px 0;
+    cursor: pointer;
+    }
+.header .menu:before {
+    content: "";
+    width: 20px;
+    height: 2px;
+    background-color: #fff;
+}
+</style>
---
+++ zhihu-daily-vue/src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Header from '@/components/Header'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [{
+        path: '/',
+        name: 'header',
+        component: Header
+    }]
+})
