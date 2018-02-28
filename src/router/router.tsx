f5f4382481adbf2171203a796845aaaf2dffedb4
ly321 <965768042@qq.com>
2018-02-28 13:52:25 +0800

    resolve refresh problem

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, HashRouter as Router, Switch } from 'react-router-dom';
 
 import  Home from '../view/home/Home';
 import ListTheme from '../view/list_theme/List_theme';
