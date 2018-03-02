7e302d4d171673bf9265b6b6ae6dc67e3e7ec4cf
ly321 <965768042@qq.com>
2018-03-02 16:17:22 +0800

    change webpack

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,9 +1,10 @@
 const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const path=require('path');
 module.exports = {
   entry: "./src/index.tsx",
   output: {
     filename: "bundle.js",
-    path: __dirname + "/dist"
+    path: path.resolve(__dirname, "../dist"),
   },
 
   devtool: "source-map",
